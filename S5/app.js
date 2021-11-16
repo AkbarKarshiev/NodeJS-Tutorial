4e6c3396741f2dc880712a3f077de151abd7b8b8
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2021-11-16 10:57:40 +0500

    70. Serving HTML Pages

--- S5/app.js
+++ S5/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-const app = express();
 
 app.use(bodyParser.urlencoded({extended: false})); 
 
@@ -12,7 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).send('<h1>Page not found</h1>');
 })
 
-app.listen(4000);
+app.listen(5000);
