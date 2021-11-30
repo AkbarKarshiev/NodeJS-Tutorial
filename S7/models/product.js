c11408e773d485e6b5939f588e998c1e96e821de
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2021-11-30 18:05:46 +0500

    103. Refactoring the File Storage Code

--- S7/models/product.js
+++ S7/models/product.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(require.main.filename), 
+  'data', 
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent))
+    }
+  });
+}
+
 module.exports = class Produdct {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename), 
-      'data', 
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -25,16 +33,6 @@ module.exports = class Produdct {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename), 
-      'data', 
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent))
-    });
+    getProductsFromFile(cb);
   }
 }
