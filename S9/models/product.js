e156748d85577e1749d55abddf22d1f666c73cd0
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2021-12-14 16:25:59 +0500

    118. Adding the Product ID to the Path

--- S9/models/product.js
+++ S9/models/product.js
@@ -27,6 +27,8 @@ module.exports = class Produdct {
   }
 
   save() {
+    this.id = Math.random().toString(); 
+
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
