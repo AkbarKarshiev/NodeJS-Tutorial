a95b5841089c3c458352cb54c8d86806a59d8d00
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-01-08 19:10:24 +0500

    173. Resetting the Cart & Fetching and Outputting Orders

--- S11/controllers/shop.js
+++ S11/controllers/shop.js
@@ -113,9 +113,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
@@ -132,21 +134,24 @@ exports.postOrder = (req, res, next) => {
         .catch(err => console.log(err));
     })
     .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    pageTitle: "Your Orders",
-    path: "/orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    pageTitle: "Checkout",
-    path: "/checkout",
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      console.log(orders);
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
 };
